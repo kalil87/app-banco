03fd08e41e82587e5b9fe3da8d39a6cd0604b7f0
Kalil Sat <kalilomarsat@gmail.com>
2026-04-13 12:36:08 -0300

    feat/1304/agrego en Cuenta los metodos: depositar, retirar y transferir. Tambien 2 metodos auxiliares

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -2,4 +2,37 @@ public class Cuenta {
     private String numero;
     private double saldo;
     private TipoCuenta tipoDeCuenta;
+
+    public Cuenta(String numero, double saldo, TipoCuenta tipo) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.tipoDeCuenta = tipo;
+    }
+
+    private double getSaldo() {
+        return saldo;
+    }
+
+    public void verDatos() {
+        System.out.println("cuenta numero: " + numero);
+        System.out.println("saldos: " + getSaldo());
+        System.out.println("tipo de cuenta: " + tipoDeCuenta + "\n");
+    }
+
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double monto) {
+        setSaldo(this.saldo + monto);
+    }
+
+    public void retirar(double monto) {
+        setSaldo(this.saldo - monto);
+    }
+
+    public void transferir(Cuenta cuentaDestino, double monto) {
+        retirar(monto);
+        cuentaDestino.depositar(monto);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //probando metodos de Cuenta
+        Cuenta cuenta1 = new Cuenta("abc1", 0, TipoCuenta.CA);
+        Cuenta cuenta2 = new Cuenta("abc2", 0, TipoCuenta.CA);
 
+        cuenta1.verDatos();
+        cuenta2.verDatos();
 
+        cuenta1.depositar(10000);
+        cuenta2.depositar(20000);
+
+        cuenta1.verDatos();
+        cuenta2.verDatos();
+
+        cuenta1.retirar(1000);
+        cuenta1.transferir(cuenta2, 4000);
+
+        cuenta1.verDatos();
+        cuenta2.verDatos();
     }
 }
